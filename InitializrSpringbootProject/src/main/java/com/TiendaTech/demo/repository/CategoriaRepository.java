a24b706484edb515f9ae3b81fb8d821f09dfeb1f
Randall Corrales Loaiza <randallcorrales05@gmail.com>
2026-06-11 21:02:16 -0600

    Semana 4

--- InitializrSpringbootProject/src/main/java/com/TiendaTech/demo/repository/CategoriaRepository.java
+++ InitializrSpringbootProject/src/main/java/com/TiendaTech/demo/repository/CategoriaRepository.java
@@ -1,13 +1,15 @@
 package com.tiendaTech.demo.repository;
 
-
 import com.tiendaTech.demo.domain.Categoria;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
     public List<Categoria> findByActivoTrue();
+
+    public List<Categoria> findById();
+
 }
- 
